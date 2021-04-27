101b89e04b9b867bd0d098ee8aa382ac4c1f1c6a
Hasanmohammadi <hassanshm97@gmail.com>
2021-04-27 17:20:23 +0430

    add pagination

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,45 @@
+// import { useHistory } from "react-router-dom";
+import { useRouter } from "next/router";
+
+import "antd/dist/antd.css";
+import { Pagination } from "antd";
 import Blog from "../components/blog";
 import styles from "../styles/Home.module.css";
 import { POSTS_URL } from "../url";
 
 export default function Home({ posts }) {
-  console.log(posts);
+  const router = useRouter();
+  function onChange(pageNumber) {
+    console.log("Page: ", pageNumber);
+    // router.push(`?page=${pageNumber}`);
+
+    router.push({
+      pathname: "/",
+      query: { page: pageNumber },
+    });
+  }
 
   return (
     <div className={styles.container}>
       {posts.map((post) => (
-        <Blog blog={post} />
+        <Blog key={post.id} blog={post} />
       ))}
+
+      <div>
+        <Pagination
+          showQuickJumper
+          defaultCurrent={1}
+          total={600}
+          onChange={onChange}
+        />
+      </div>
     </div>
   );
 }
 
-export const getServerSideProps = async (ctx) => {
-  const response = await fetch(POSTS_URL);
+export const getServerSideProps = async (context) => {
+  const { page } = context.query;
+  const response = await fetch(`${POSTS_URL}${page}`);
   const data = await response.json();
 
   return {
--- src/url.js
+++ src/url.js
@@ -1,2 +1,2 @@
-export const POSTS_URL = "https://api.bourseon.com/posts?take=12&page=1"
+export const POSTS_URL = "https://api.bourseon.com/posts?take=12&page="
 export const DETAILLE_POST_URL = "https://api.bourseon.com/posts/"
